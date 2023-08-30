57edb6e2296f559b188da60d5809b3fb9d369dd9
Connor O'Brien <connorbobrien@gmail.com>
2023-08-30 08:29:09 -0300

    Debounce api calls

--- js/handlers/listings/handleSearch.js
+++ js/handlers/listings/handleSearch.js
@@ -2,12 +2,15 @@ import { search } from "../../api/listings/search.js";
 import displaySearchResults from "../../ui/listings/displaySearchResults.js";
 import hideSearchResults from "../../ui/listings/hideSearchResults.js";
 import displaySearchError from "../../ui/listings/displaySearchError.js";
+import debounce from "../../helpers/debounce.js";
 
 export default function handleSearch() {
 	const input = document.querySelector("#search");
 
-	input.addEventListener("input", doSearch);
+	input.addEventListener("input", debouncedSearch);
 }
+
+const debouncedSearch = debounce(doSearch, 500);
 
 async function doSearch(event) {
 	const tag = event.target.value.trim();
